bb8950300692a397d8039fc4f7921ece2a2939bd
Sean Erickson <seanericksonpl12@gmail.com>
2024-09-27 08:06:01 -0400

    updated injected

--- Sources/Injector/Injected.swift
+++ Sources/Injector/Injected.swift
@@ -12,36 +12,12 @@ public class Injected<Value: Dependency> {
     
     @usableFromInline
     let keyPath: KeyPath<EnvironmentValues, Value>
-
-    @available(*, unavailable)
-    public var wrappedValue: Value {
-        get { fatalError("This wrapper only works on instance properties of classes") }
-        set { fatalError("This wrapper only works on instance properties of classes") }
-    }
+    let environment = EnvironmentValues()
+    lazy var _wrappedValue = GlobalResolver.shared.resolvers[keyPath]?.resolve(for: Value.self) ?? environment[keyPath: keyPath] ?? environment[Value.Key.self] as! Value
     
-    @inlinable
-    public static subscript<M: Injectable>(
-        _enclosingInstance instance: M,
-        wrapped wrappedKeyPath: ReferenceWritableKeyPath<M, Value>,
-        storage storageKeyPath: ReferenceWritableKeyPath<M, Injected>
-    ) -> Value {
-        get {
-            let dependency = instance[keyPath: storageKeyPath]
-            if let value = instance.resolvers[dependency.keyPath]?.resolve(for: Value.self) ?? instance.environment[Value.Key.self] as? Value {
-                return value
-            } else if let value = instance.environment[keyPath: dependency.keyPath] as? Value.Key.Value {
-                instance.environment[Value.Key.self] = value
-            }
-            
-            return instance.environment[Value.Key.self] as! Value
-        }
-        set {
-            print("*** WARNING! ***\nYou are setting an Injected Value manually. This will be overridden by an Injection!")
-            let dependency = instance[keyPath: storageKeyPath]
-            if let value = instance.environment[keyPath: dependency.keyPath] as? Value.Key.Value {
-                instance.environment[Value.Key.self] = value
-            }
-        }
+    public var wrappedValue: Value {
+        get { _wrappedValue }
+        set { _wrappedValue = newValue }
     }
     
     @inlinable
